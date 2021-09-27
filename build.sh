8e35c89af3f07dd49cfdf11372b7bb05f2a9e8a5
lanying <616172@33e9.com>
2021-09-27 14:25:28 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 # 只需修改版本号，即新建对应版本的镜像和容器
 # 暂不指定端口，方便在 portainer 处理版本和端口
 
+# command result to var 'tag'
+tag=$(date +%Y_%m_%d_%H_%M_%S)
+
 # 构建镜像
-docker build -t blog:1.0.1 .
+docker build -t blog:${tag} .
 
 # 启动容器
 docker run \
     -itd \
-    --name blogV1.0.1 \
+    --name blog_${tag} \
     --restart always \
-    blog:1.0.1
+    blog:${tag}
 
 # 进入容器
 # docker exec -i -t blog /bin/bash
